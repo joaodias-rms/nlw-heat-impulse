724a42c28414241e63f47dd60f1ad0605cb3c036
joao <joao.victor@topsapp.com.br>
2021-10-21 09:51:09 -0400

    criada rota de envio de mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import {Router} from 'express'
 import { AuthUserController } from './controller/AuthUserController'
+import { CreateMessageController } from './controller/CreateMessageController'
+import { EnsureAuthenticated } from './middleware/EnsureAuthenticated'
 
 const router= Router()
 
 router.post("/authenticate", new AuthUserController().handle)
+
+router.post("/message", EnsureAuthenticated ,new CreateMessageController().handle)
 
 
 export {router}
