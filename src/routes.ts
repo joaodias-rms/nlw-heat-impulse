b8a0fc7ab5d9e38d984115ca3632d34ff7241d18
joao <joao.victor@topsapp.com.br>
2021-10-21 10:40:46 -0400

    Adicionada rota de perfil

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import {Router} from 'express'
 import { AuthUserController } from './controller/AuthUserController'
 import { CreateMessageController } from './controller/CreateMessageController'
+import { GetLast3MessagesController } from './controller/GetLast3MessagesControllers'
+import { ProfileUserController } from './controller/ProfileUserController'
 import { EnsureAuthenticated } from './middleware/EnsureAuthenticated'
 
 const router= Router()
@@ -9,5 +11,8 @@ router.post("/authenticate", new AuthUserController().handle)
 
 router.post("/message", EnsureAuthenticated ,new CreateMessageController().handle)
 
+router.get("/message/last3", new GetLast3MessagesController().handle)
+
+router.get("/profile", EnsureAuthenticated ,new ProfileUserController().handle)
 
 export {router}
